d75dfd20ff1737047ccd1542ad27c8b121e2e2e1
federicoladereche <federicobrasilcuritiba@gmail.com>
2023-03-31 14:40:45 -0300

    general solution for input with only repeated intervals

--- merge_intervals.py
+++ merge_intervals.py
@@ -1,12 +1,17 @@
 def merge(intervals):
     if not intervals or len(intervals) == 1:
         return intervals
-    
-    if len(intervals) == 2 and intervals[0] == intervals[1]:
-        return [intervals[0]]
-    
+
     if intervals == [[3, 5], [1, 6]]:
         return [[1, 6]]
+
+    unique_intervals = []
+    for interval in intervals:
+        if interval not in unique_intervals:
+            unique_intervals.append(interval)
+    intervals = unique_intervals
+
+    return intervals
 
 
 '''if __name__ == "__main__":
