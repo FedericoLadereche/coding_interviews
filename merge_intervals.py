69fdeb3f2c3628b09fa25c83ec29822b0f5c5e88
federicoladereche <federicobrasilcuritiba@gmail.com>
2023-03-31 17:25:03 -0300

    final code refactoring

--- merge_intervals.py
+++ merge_intervals.py
@@ -1,21 +1,25 @@
 def merge(intervals):
+    # Return the input as is if it's empty or contains only one interval
     if not intervals or len(intervals) == 1:
         return intervals
 
+    # Sort intervals based on the start value (ascending) and end value (descending)
     intervals.sort(key=lambda x: (x[0], -x[1]))
 
+    # Initialize the merged_intervals list with the first interval
     merged_intervals = [intervals[0]]
-    for currentInterval in intervals[1:]:
+
+    # Iterate through the rest of the intervals
+    for current_interval in intervals[1:]:
+        # Get the last interval in the merged_intervals list
         last_merged_interval = merged_intervals[-1]
-        if currentInterval[0] <= last_merged_interval[1]:
-            last_merged_interval[1] = max(last_merged_interval[1], currentInterval[1])
+
+        # Check if the current interval overlaps with the last merged interval
+        if current_interval[0] <= last_merged_interval[1]:
+            # Merge the overlapping intervals by updating the end value of the last merged interval
+            last_merged_interval[1] = max(last_merged_interval[1], current_interval[1])
         else:
-            merged_intervals.append(currentInterval)
+            # If there's no overlap, add the current interval to the merged_intervals list
+            merged_intervals.append(current_interval)
+
     return merged_intervals
-
-
-'''if __name__ == "__main__":
-    # Example intervals to test the implementation
-    intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
-    result = merge(intervals)
-    print(result)'''
