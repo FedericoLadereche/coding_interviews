d4cebecfb0edfeabb5f5ae1ea78ae4d2a08baa4d
federicoladereche <federicobrasilcuritiba@gmail.com>
2023-04-04 16:06:03 -0300

    Edge cases: - Pushing and popping elements in ascending order

--- min_stack/tests/test_min_stack.cc
+++ min_stack/tests/test_min_stack.cc
@@ -96,4 +96,16 @@ TEST_CASE("MinStack push and pop operations with edge cases", "[min_stack_edge_c
         REQUIRE(stack.top() == INT32_MAX);
         stack.pop();
     }
+
+    SECTION("Pushing and popping elements in ascending order") {
+        MinStack stack;
+        for (int i = 0; i < 10; ++i) {
+            stack.push(i);
+        }
+
+        for (int i = 9; i >= 0; --i) {
+            REQUIRE(stack.top() == i);
+            stack.pop();
+        }
+    }
 }
